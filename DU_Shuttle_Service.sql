3de27160e6d37e409c726645db286c50912f3fcd
aditto007 <adittoraihan19@gmail.com>
2025-02-01 16:02:21 +0600

    Add files via upload

---
+++ DU_Shuttle_Service.sql
@@ -0,0 +1,28 @@
+create database dusuttleservice;
+
+use dusuttleservice;
+
+create table createanaccout1(
+ name varchar(20),
+regno varchar(20),
+hallname varchar(20),
+deptname varchar(20),
+birthofdate varchar(20),
+gender varchar(20),
+resudent varchar(20),
+email varchar(50),
+mobileno varchar(20),
+bloodgrroup varchar(20),
+password varchar(20)
+);
+
+select * from createanaccout1;
+
+create table bank (regno varchar(20),
+date varchar(60),
+type varchar(20),
+amount varchar(20),
+ticket varchar(20)
+);
+
+select *from bank;
